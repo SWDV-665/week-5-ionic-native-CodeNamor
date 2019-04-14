9cb7e196f94b45b616ecb4ccc9ca70f7a63efbec
F Derek Roman <derek.roman81@gmail.com>
2019-04-13 21:21:10 -0500

    Initial Commit
    Initial Commit for Week 5

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,136 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  constructor(public alertController: AlertController, public toastCtrl: ToastController) {}
+
+  title = "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: "1"
+    },
+    {
+      name: "Eggs",
+      quantity: "Dozen"
+    },
+    {
+      name: "Orange Juice",
+      quantity: "1"
+    },
+    {
+      name: "Avacado",
+      quantity: "3"
+    },
+  ];
+
+  addItem() {
+    console.log("Adding Item:")
+    this.showItemPromt();
+  }
+
+  async showItemPromt() {
+    const alert = await this.alertController.create({
+      header: 'Add item',
+      message: "Please enter Item Content",
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          type: 'text',
+          placeholder: 'quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: item => {
+            console.log('Confirm Ok', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Removing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1)
+  }
+
+  async editItem(item, index) {
+    console.log("Edit Item - ", item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Editing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+    this.showEditItemPromt(item, index);''
+
+  }
+
+  async showEditItemPromt(item, index) {
+    const alert = await this.alertController.create({
+      header: 'Edit item',
+      message: "Please edit Item ...",
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Name',
+          value: item.name
+        },
+        {
+          name: 'quantity',
+          type: 'text',
+          placeholder: 'quantity',
+          value: item.quantity
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: item => {
+            console.log('Confirm Ok', item);
+            this.items[index] = item;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+}
